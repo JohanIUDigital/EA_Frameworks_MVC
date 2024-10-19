1604502cfe54aa8cf0c0cb6cbd6c1dfc7d2ce9e1
AlexisHolguinJDL <alexis.holguin@jdlsas.com>
2024-10-19 14:47:28 -0500

    Creacion EA Frameworks MVC

---
+++ src/test/java/edu/iud/floristeriaiud/FloristeriaiudApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iud.floristeriaiud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloristeriaiudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
